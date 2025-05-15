e2572a838504abafbf55194e42ab92ea2f557263
Roel Navarrete <89426660+roenavarrete@users.noreply.github.com>
2025-05-15 02:05:49 -0500

    Fixed kraken attack sound not playing

--- Assets/Scripts/Kraken/KrakenHitbox.cs
+++ Assets/Scripts/Kraken/KrakenHitbox.cs
@@ -11,13 +11,11 @@ public class KrakenHitbox : MonoBehaviour
     public bool isHitOnce = false;
 
     private PlayerStatManager statManager;
-    private AudioSource audioSource;
 
     private void Start()
     {
         GameObject player = GameObject.Find("GamePlayer");
         statManager = player.GetComponent<PlayerStatManager>();
-        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerStay(Collider other)
@@ -25,7 +23,6 @@ public class KrakenHitbox : MonoBehaviour
         if (other.CompareTag("Player") && isHitboxActive && !isHitOnce)
         {
             Debug.Log("Attack1AllTentacleStab hits!");
-            audioSource.Play();
             statManager?.TakeDamage(damageAmount);
             isHitOnce = true;
         }
